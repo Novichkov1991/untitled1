3c5ae6937f68f42976e54b97e9852e7f825621dd
Novichkov1991 <novichkov-1991@list.ru>
2023-06-28 21:30:16 +0300

    + 2 задачу

--- src/cikles3.java
+++ src/cikles3.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
-
 //public class cikles3 {
 //    public static void main(String[] args) {
-//        for (int x = 10; x < 1000000; x++) {
-//            for (; x > 0; x/10) {
-//                for ()
+//        int i = 0;
+//        int x;
+//        while (x > 0) {
+//            x/=10;
+//            i++;
 //
-//            }
 //        }
 //    }
 //}
+
 public class cikles3 {
     public static void main(String[] args) {
-        int i = 0;
-        int x;
-        while (x > 0) {
-            x/=10;
-            i++;
+        for (int x =10; x < 1000000; x++) {
+            int i = 0;
+            while (x > 0) {
+                x /= 10;
+                i++;
+
+            }
         }
     }
 }
---
+++ src/cikles5.java
@@ -0,0 +1,29 @@
+//public class cikles5 {
+//    public static void main(String[] args) {
+//        for (int i = 100; i<1000; i++) {
+//            int number = i;
+//            int step = 0;
+//            while (number>0){
+//                int tempCifra = number%10;
+//                int stepTemp = 0;
+//                boolean isUnique = true;
+//                int numberTemp = number;
+//                while (numberTemp>0){
+//                    int tempNumber1 = numberTemp%10;
+//                    if (stepTemp == step){
+//                        continue;
+//                    }
+//                    if(tempNumber1 != tempCifra) {
+//                        isUnique = false;
+//                        break;
+//                    }
+//                    number/=10;
+//                }
+//                number/=10;
+//            }
+//            if () {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
